41ab942be3125cce9b74f335891569545684e936
armaa <armaa@Klair>
2024-09-11 23:33:36 -0600

    First commit (Boilerplate spring boot app)

---
+++ realtime-mousepointer-service/src/test/java/com/realtime/mousepointer/service/RealtimeMousepointerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realtime.mousepointer.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeMousepointerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
